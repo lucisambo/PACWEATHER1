77222a9102f48c533cc552442fe50b3b232aae55
lucisambo <lucillesambo.de@gmail.com>
2022-08-27 18:02:29 +0200

    css
    removed width in weather forecast

--- src/app.js
+++ src/app.js
@@ -47,9 +47,9 @@ function displayForecast(response) {
                   <div class="weather-forecast-temperatures">
           <span class="weather-forecast-temperature-max"> ${Math.round(
             forecastDay.temp.max
-      )}°</span>
+          )}°</span>
       <span class="weather-forecast-temperature-min"> ${Math.round(
-            forecastDay.temp.min
+        forecastDay.temp.min
       )}°</span>
       </div>
         <img
